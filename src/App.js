0fdbb0093423b965f7679707ae1f022d1bbbdc70
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-22 01:04:57 +0200

    moved the state a tiny little step further

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Alpy'
+      name: { firstName: 'Alpy', lastName: 'Tarkan' },
+      company: 'ZTM'
     }
   }
 
@@ -18,10 +19,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}.
           </p>
           <button onClick={() => {
-            this.setState({ name: 'Endo' })
+            this.setState({ name: { firstName: 'Endo', lastName: 'Tarkanson' }, company: 'Tech Norway AS' })
+            console.log(this.state)
           }}>Change Name</button>
         </header>
       </div>
