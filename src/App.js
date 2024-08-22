51fdd268039cacf7109d106a1fdb7e7be7e86b51
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-22 02:19:36 +0200

    added a simple API call and displayed that simple response in the UI

--- src/App.js
+++ src/App.js
@@ -8,28 +8,17 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '12e12313'
-        },
-        {
-          name: 'Frank',
-          id:'12ed2dacas'
-        },
-        {
-          name: 'Jacky',
-          id:'1231e'
-        },
-        {
-          name: 'Andrei',
-          id:'12e1e213'
-        },
-      ]
-
+      monsters: []
     }
   }
-
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+        () => console.log(this.state)))
+  };
   render() {
     return (
       <div className="App">
