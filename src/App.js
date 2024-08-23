9f0de23b8e17f336e8d6afb57fbebbd9cd9ccfb2
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-23 14:30:41 +0200

    added a basic search box

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
-    console.log('1')
+      monsters: [],
+    };
+    console.log("constructor");
   }
   componentDidMount() {
-    console.log('3')
-    fetch('https://jsonplaceholder.typicode.com/users')
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users }
-      },
-        () => console.log(this.state)))
-  };
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
+  }
   render() {
-    console.log('2')
+    console.log("render");
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}>
-              <h1>{monster.name}</h1></div>
-          })
-        }
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+              console.log(event.target.value)
+          }}
+        />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
